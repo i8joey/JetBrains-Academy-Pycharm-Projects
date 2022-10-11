333a8cb2c41756148ffa4d20f83b8db81a370d08
i8joey <77411739+i8joey@users.noreply.github.com>
2022-10-10 18:20:33 -0700

    Update key_terms.py

--- Key Term Extraction/key_terms.py
+++ Key Term Extraction/key_terms.py
@@ -1,11 +1,12 @@
 import string
 from lxml import etree
 from nltk.tokenize import word_tokenize
-from collections import Counter
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-
+# getting the XML file
 news = "news.xml"
 root = etree.parse(news).getroot()
 article = root[0]
@@ -13,6 +14,7 @@ article_head = []
 article_text = []
 article_counter = {}
 lemmatizer = WordNetLemmatizer()
+# extracting the titles and articles from XML
 for i in article:
     for x in i:
         if x.get("name") == "head":
@@ -20,20 +22,43 @@ for i in article:
         else:
             article_text.append(x.text)
 for i in range(len(article_text)):
+    # tokenizing text
     tokenized = word_tokenize(article_text[i].lower())
     lem = []
+    tagged = []
     for x in tokenized:
+        # lemmatizing text and only keeping the nouns
         lem.append(lemmatizer.lemmatize(x, pos="n"))
     for y in lem:
-        if y in stopwords.words("english") or y in list(string.punctuation):
+        # removing unnecessary words
+        if y in stopwords.words("english") or y in list(string.punctuation) or y in ['ha', 'wa', 'u', 'a']:
             while y in lem:
                 lem.remove(y)
-    article_counter[article_head[i]] = Counter(lem).most_common(10)
+    for k in lem:
+        # individually tagging each word without using context to better filter nouns
+        if nltk.pos_tag([k])[0][1] == "NN":
+            tagged.append(k)
+    article_counter[article_head[i]] = tagged
     lem = []
-sorted_dict = {d: (sorted(k, key=lambda x: (x[1], x[0]), reverse=True))
-               for d, k in article_counter.items()}
-for i in sorted_dict.items():
-    print(i[0] + ":")
-    for x in range(5):
-        print(i[1][x][0], end=" ")
+    tagged = []
+
+# creating TF-IDF matrix 
+dataset = [" ".join(x for x in y) for y in article_counter.values()]
+vectorizer = TfidfVectorizer(use_idf=True)
+matrix = vectorizer.fit_transform(dataset)
+list1 = []
+vect_dict = {}
+# matching the TF-IDF values from the matrix to its words and articles
+for i in range(10):
+    vect_dict[article_head[i]] = dict(zip(vectorizer.get_feature_names_out(), matrix.toarray()[i]))
+# sorting the values inversely by TF-IDF and if values are same, sorts alphabetically 
+sort_vect = {d: (sorted(k.items(), key=lambda x: (x[1], x[0]), reverse=True))
+             for d, k in vect_dict.items()}
+# print 5 words with the highest TF-IDF score in each article
+for i in sort_vect.items():
+    print(f"{i[0]}:")
+    for y, x in enumerate(i[1]):
+        print(x[0], end=" ")
+        if y == 4:
+            break
     print("\n")
